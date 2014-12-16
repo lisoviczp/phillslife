1adf5ee22902483587588077c0842878557652ed
lisoviczp <phillip.lisovicz@gmail.com>
2014-12-15 22:44:46 -0500

    fixed new picture form

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -16,7 +16,7 @@ class PicturesController < ApplicationController
 
   # GET /pictures/new
   def new
-    @picture = Picture.new(picture_params) 
+    @picture = Picture.new
   end
 
   # GET /pictures/1/edit
