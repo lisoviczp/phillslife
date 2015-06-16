25f01f09fd131f88539c4cb243c817249d022367
lisoviczp <phillip.lisovicz@gmail.com>
2015-06-16 17:52:19 -0400

    reversing items

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -5,7 +5,7 @@ class PicturesController < ApplicationController
   # GET /pictures
   # GET /pictures.json
   def index
-    @pictures = Picture.all.reverse
+    @pictures = Picture.all
   end
 
   # GET /pictures/1
