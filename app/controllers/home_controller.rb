1a1a198d587a17fe2f45a0130fcecf9cffc5023b
lisoviczp <phillip.lisovicz@gmail.com>
2015-09-11 16:51:51 -0400

    page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,5 @@
 class HomeController < ApplicationController
   def index
-  	@pictures = Picture.paginate(:page => params[:page], :per_page => 5)
+  	@pictures = Picture.paginate(:page => params[:page], :per_page => 16)
   end
 end
