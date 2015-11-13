daa5979cd07c7b5643a4e21ac0947f5174d7d2b1
lisoviczp <phillip.lisovicz@gmail.com>
2015-11-13 16:25:23 -0500

    hav2getmasonry up again

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
-  	@pictures = Picture.paginate(:page => params[:page], :per_page => 20)
+  	@pictures = Picture.paginate(:page => params[:page], :per_page => 1)
     respond_to do |format|
       format.html
       format.js
