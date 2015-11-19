3fac55eb448d8dcf556399c9ffc2485a847f3982
lisoviczp <phillip.lisovicz@gmail.com>
2015-11-19 18:33:10 -0500

    fixed pic show and img url link

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
-  	@pictures = Picture.paginate(:page => params[:page], :per_page => 20)
+  	@pictures = Picture.paginate(:page => params[:page], :per_page => 12)
     respond_to do |format|
       format.html
       format.js
