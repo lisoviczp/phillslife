c4766463f472309746d0fec00da665368cc2ef26
lisoviczp <phillip.lisovicz@gmail.com>
2015-11-19 18:54:34 -0500

    trying to finalize..

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -56,7 +56,7 @@ $(document).ready(function() {
   if ($('.pagination').length) {
     $(window).scroll(function() {
       var url = $('.pagination .next_page').attr('href');
-      if (url && $(window).scrollTop() > $(document).height() - $(window).height() - 50) {
+      if (url && $(window).scrollTop() > $(document).height() - $(window).height() - 100) {
         $('.pagination').text("Please Wait...");
 		$('#picture-container').masonry( 'reloadItems' );
         return $.getScript(url);
